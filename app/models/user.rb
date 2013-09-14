e19a40eb4c008dbb88381dc574a788adc78fa54f
Jesse Appel <jesseappel@gmail.com>
2013-09-14 14:26:02 -0400

    styled the flash messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,11 +12,11 @@ attr_accessible :user_attributes
 validates :first_name, presence: true
 validates :last_name, presence: true
 validates :profile_name, presence: true,
-						uniqueness: true,
-						format:{
-							with: /a-zA-Z0-9_-/, 
-							message: 'Must be formatted correctly.'
-						}
+						uniqueness: true
+						#format:{
+						#	with: /a-zA-Z0-9_-/, 
+						#	message: 'Must be formatted correctly.'
+						#}
 end 
      def full_name
      	first_name + " " +last_name
